bd4197ab9735e02a3f052a41f15d5206df9bd868
WhozKunal <ks286823@gmail.com>
2025-03-01 16:40:46 +0530

    code

---
+++ merge_shorted_array.py
@@ -0,0 +1,8 @@
+#You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, 
+#and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+
+
+def merger_array():
+    pass
+
+    
